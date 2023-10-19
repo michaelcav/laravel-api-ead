a2c742366ba92943f951fca4f8eadc5d0385df02
Michael Carvalho <michaeldougmk@gmail.com>
2023-10-19 17:44:13 -0300

    Auth Test

--- tests/Feature/Api/Auth/AuthTest.php
+++ tests/Feature/Api/Auth/AuthTest.php
@@ -2,20 +2,68 @@
 
 namespace Tests\Feature\Api\Auth;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Tests\Feature\Api\UtilsTrait;
 
 class AuthTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
+    use RefreshDatabase;
+    use UtilsTrait;
+
+    public function test_fail_auth()
     {
-        $response = $this->get('/');
+        $response = $this->postJson('/auth', []);
+
+        $response->assertStatus(422);
+    }
+
+    public function test_auth()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->postJson('/auth', [
+            'email' => $user->email,
+            'password' => 'password',
+            'device_name' => 'test'
+        ]);
+
+        $response->assertStatus(200);
+    }
+
+    public function test_error_logout()
+    {
+        $response = $this->postJson('/logout');
+
+        $response->assertStatus(401);
+    }
+
+    public function test_logout()
+    {
+        $token = $this->createTokenUser();
+
+        $response = $this->postJson('/logout', [], [
+            'Authorization' => "Bearer {$token}",
+        ]);
+
+        $response->assertStatus(200);
+    }
+
+    public function test_error_get_me()
+    {
+        $response = $this->getJson('/me');
+
+        $response->assertStatus(401);
+    }
+
+    public function test_get_me()
+    {
+        $token = $this->createTokenUser();
+
+        $response = $this->getJson('/me', [
+            'Authorization' => "Bearer {$token}",
+        ]);
 
         $response->assertStatus(200);
     }
